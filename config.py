d320ee30421ef3248fd656d6ecfa51bd3a923dd5
VIRUS <virus_myaseenvirus@yahoo.com>
2022-05-30 10:13:29 +0500

    Config file UPDATED

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ class Config:
     MONGO_DB = os.environ.get('MONGO_DB')
     MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
     MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
-    MONGO_URI = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.8njfgac.mongodb.net/{MONGO_DB}?retryWrites=true&w=majority"
+    MONGO_CLUSTER = os.environ.get(‘MONGO_CLUSTER’)
+    MONGO_CLUSTER_ID = os.environ.get(‘MONGO_CLUSTER_ID’)
+    MONGO_URI = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}.{MONGO_CLUSTER_ID}.mongodb.net/{MONGO_DB}?retryWrites=true&w=majority"
